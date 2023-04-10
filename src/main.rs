e2ca4eb81e9e163853cd3f903ef6a20ebd23db23
Nelliguns <liamnelligan000@gmail.com>
2023-04-10 18:11:04 +0200

    removed rocket as depency for project

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-#[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+fn main() {
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
 }
