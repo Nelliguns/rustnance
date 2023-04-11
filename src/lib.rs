d3912d925e6e9425d36681cfc03a373a732ea698
Nelliguns <liamnelligan000@gmail.com>
2023-04-11 07:09:47 +0200

    intrinsic value calculations correct, added test for margin of safety fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
 // test
 #[cfg(test)]
 mod tests {
-    use crate::value::calculate_intrinsic_value;
+    use crate::value::{calculate_intrinsic_value, margin_of_safety};
 
     #[test]
     fn growth_rate_test() {
-        let input = vec![1.0, 1.1, 1.2, 1.3, 1.4];
-        let result = calculate_intrinsic_value(input, 0.08);
-        assert_eq!(result, 5.0);
+        let cashflow = vec![14641.0, 16105.0, 17716.0, 19487.0, 21436.00];
+        let result = calculate_intrinsic_value(cashflow, 0.15);
+        assert_eq!(result, 306674.7);
+    }
+    #[test]
+    fn margin_of_safety_test() {
+        let value = 10000.0;
+        let safety_margin = 0.3;
+        let result = margin_of_safety(value, safety_margin);
+        assert_eq!(result, 7000.0);
     }
 }
 
@@ -34,58 +41,46 @@ mod value {
         }
 
         let avg_growth_rate: f32 = (all_growth_rates.iter().sum::<f32>() as f32) / all_growth_rates.len() as f32;
-        println!("avg growth: {}", avg_growth_rate);
         return avg_growth_rate;
     }
 
     fn calculate_future_cashflow(cash_flow_list: &Vec<f32>) -> Vec<f32> {
-        // Should create a new list based on the growth rate and the last item of cash flow list
         let growth_rate = calculate_growth_rate(cash_flow_list);
         
         let mut future_cashflow_list: Vec<f32> = Vec::new();
         let mut cashflow: f32 = cash_flow_list[cash_flow_list.len() - 1] as f32;
-        for i in 0..5 {
-            println!("This is i: {}", i);
-            
+        for _ in 0..10 {
             future_cashflow_list.push(cashflow * growth_rate);
             cashflow *= growth_rate;
         }
-        println!("Future cashflow: {:?}", future_cashflow_list);
-        println!("Length of f cashflow list: {}", future_cashflow_list.len());
         return future_cashflow_list
     }
 
     pub fn calculate_intrinsic_value(cash_flow_list: Vec<f32>, r: f32) -> f32 {
     
-        let growth_rate = calculate_growth_rate(&cash_flow_list);
         let future_cash_flow_list = calculate_future_cashflow(&cash_flow_list);
     
         let mut discounted_cashflow = 0.0;
         let mut discounted_cashflow_list: Vec<f32> = Vec::new();
-        let mut n = 0;
+        let mut total_free_cashflow = 0.0;
         
+        let mut n = 0;
         for cashflow in &future_cash_flow_list {
             n += 1;
             discounted_cashflow = cashflow / (1.0 + &r).powf(n as f32); // have another look to make sure this is correct
             discounted_cashflow_list.push(discounted_cashflow);
         }
-        println!("Discounted cashflow: {:?}", discounted_cashflow_list);
-        println!("Final year cashflow: {}", (future_cash_flow_list[future_cash_flow_list.len() - 1]));
-        println!("Discount rate - growth rate: {}", (&r - growth_rate));
-        
-        //                                              [Final Year FCF * (1 + Perpetuity Growth Rate)] ÷ (Discount Rate – Perpetuity Growth Rate)
-        // Have another look to make sure these calculations are good.
-        let mut total_free_cashflow = 0.0;
         for cashflow in &discounted_cashflow_list {
             total_free_cashflow += cashflow;
         }
-        println!("Total free cash flow: {}", total_free_cashflow);
 
         let intrisic_value = total_free_cashflow + (discounted_cashflow_list[discounted_cashflow_list.len() - 1]) * 10.0;
-        
-        println!("This is added to discounted cashflow: {}", ((discounted_cashflow_list[discounted_cashflow_list.len() - 1]) * 10.0));
 
         return intrisic_value
+    }
+
+    pub fn margin_of_safety(intrinsic_value: f32, safety_margin: f32) -> f32 {
+        intrinsic_value * (1.0 - safety_margin)
     }
     
     pub fn intrinsic_value_per_stock(intrinsic_value: f32, outstanding_shares: u32) -> f32 {
