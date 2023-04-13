b8e606d83cdc399b9a8897b6a87b1d7b2a9956a6
Nelliguns <liamnelligan000@gmail.com>
2023-04-13 06:54:47 +0200

    Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,59 @@ mod tests {
 
 
 mod value {
-
+    
+    /// Performs calculations to estimate the intrinsic value of a company
+    /// 
+    /// # Arguments
+    /// 
+    /// * 'cash_flow_list' - A Vector(collection of values) that contains the free cash flow of a company
+    /// 
+    /// * 'r' - The expected percentual return, eg 15% -> 0.15
+    pub fn calculate_intrinsic_value(cash_flow_list: Vec<f32>, r: f32) -> f32 {
+    
+        let future_cash_flow_list = calculate_future_cashflow(&cash_flow_list);
+    
+        let mut discounted_cashflow = 0.0;
+        let mut discounted_cashflow_list: Vec<f32> = Vec::new();
+        let mut total_free_cashflow = 0.0;
+        
+        let mut n = 0;
+        for cashflow in &future_cash_flow_list {
+            n += 1;
+            discounted_cashflow = cashflow / (1.0 + &r).powf(n as f32); // have another look to make sure this is correct
+            discounted_cashflow_list.push(discounted_cashflow);
+        }
+        for cashflow in &discounted_cashflow_list {
+            total_free_cashflow += cashflow;
+        }
+    
+        let intrisic_value = total_free_cashflow + (discounted_cashflow_list[discounted_cashflow_list.len() - 1]) * 10.0;
+    
+        return intrisic_value
+    }
+    
+    /// Returns an estimated share price based on intrinisc value
+    /// 
+    /// # Arguments
+    /// 
+    /// * 'intrinsic_value' - An estimation of the value of a business
+    /// 
+    /// * 'outstanding_shares' - The number of shares a business has issued
+    pub fn intrinsic_value_per_stock(intrinsic_value: f32, outstanding_shares: u32) -> f32 {
+        return intrinsic_value / outstanding_shares as f32
+    }
+    
+    /// Applies a margin of safety to the intrinsic value to take account for inaccuracies in estimations
+    /// 
+    /// # Arguments
+    /// 
+    /// * 'intrinsic_value' - An estimation of the value of a business
+    /// 
+    /// * 'safety_margin' - The percentual margin, e.g. 30% -> 0.3
+    pub fn margin_of_safety(intrinsic_value: f32, safety_margin: f32) -> f32 {
+        intrinsic_value * (1.0 - safety_margin)
+    }
+    
     fn calculate_growth_rate(cash_flow_list: &Vec<f32>) -> f32 {
         
         let mut all_growth_rates: Vec<f32> = Vec::new();
@@ -54,37 +106,6 @@ mod value {
             cashflow *= growth_rate;
         }
         return future_cashflow_list
-    }
-
-    pub fn calculate_intrinsic_value(cash_flow_list: Vec<f32>, r: f32) -> f32 {
-    
-        let future_cash_flow_list = calculate_future_cashflow(&cash_flow_list);
-    
-        let mut discounted_cashflow = 0.0;
-        let mut discounted_cashflow_list: Vec<f32> = Vec::new();
-        let mut total_free_cashflow = 0.0;
-        
-        let mut n = 0;
-        for cashflow in &future_cash_flow_list {
-            n += 1;
-            discounted_cashflow = cashflow / (1.0 + &r).powf(n as f32); // have another look to make sure this is correct
-            discounted_cashflow_list.push(discounted_cashflow);
-        }
-        for cashflow in &discounted_cashflow_list {
-            total_free_cashflow += cashflow;
-        }
-
-        let intrisic_value = total_free_cashflow + (discounted_cashflow_list[discounted_cashflow_list.len() - 1]) * 10.0;
-
-        return intrisic_value
-    }
-
-    pub fn margin_of_safety(intrinsic_value: f32, safety_margin: f32) -> f32 {
-        intrinsic_value * (1.0 - safety_margin)
-    }
-    
-    pub fn intrinsic_value_per_stock(intrinsic_value: f32, outstanding_shares: u32) -> f32 {
-        return intrinsic_value / outstanding_shares as f32
-    }
+    }    
     
 }
