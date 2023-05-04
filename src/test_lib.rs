87ba869ffe392921e323b6f88d61a70578ad3faa
Liam Nelligan <liamnelligan000@gmail.com>
2023-05-04 16:23:16 +0200

    function to calculate total paid amount for payment plan is functional and passes tests

--- src/test_lib.rs
+++ src/test_lib.rs
@@ -1,5 +1,6 @@
 use crate::value::{calculate_intrinsic_value, margin_of_safety};
 
+
 #[test]
 fn growth_rate_test() {
     let cashflow = vec![14641.0, 16105.0, 17716.0, 19487.0, 21436.00];
@@ -14,7 +15,7 @@ fn margin_of_safety_test() {
     assert_eq!(result, 7000.0);
 }
 
-use crate::utilities::calculate_compounded_interest;
+use crate::utilities::{calculate_compounded_interest, cost_from_payment_plan};
 
 #[test]
 fn calculate_compounded_interest_test() {
@@ -31,3 +32,14 @@ fn calculate_compounded_interest_test() {
 
     assert_eq!(result, (660248.8360031071, 292248.83600310725));
 }
+
+#[test]
+fn cost_from_payment_plan_test() {
+    let loaned_amount: f64 = 1000000.0;
+    let interest_rate: f64 = 5.14;
+    let amortization_rate: f64 = 2.0;
+    let result = cost_from_payment_plan(&loaned_amount, &interest_rate, &amortization_rate);
+    assert_eq!(result, 2287141.6666667084)
+}
+
+
